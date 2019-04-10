e305eeee02945b30ecadcaf8ddb336b4e4f465c5
Ben Chuang <bschuang@users.noreply.github.com>
2019-04-10 01:42:53 -0400

    Added async read from database functionality

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const tmi = require('tmi.js');
 const MongoClient = require('mongodb').MongoClient;
 const cfg = require('./cfg');
+const uri = `mongodb+srv://${cfg.getDbUser()}:${cfg.getDbToken()}@tabs-hoskn.mongodb.net/test?retryWrites=true`;
 
 // BOT FUNCTIONS
 function rollDice() {
@@ -8,90 +9,108 @@ function rollDice() {
   return Math.floor(Math.random() * sides) + 1;
 }
 
-function make(coll) {
-  collection.insertOne({
-    'uid': 'test',
-    'credits': 1
-  }, function (err, res) {
-    if (err) throw err;
-    console.log("Added");
+function make(name) {
+  writeToDb("users", {
+    'uid': name,
+    'credits': 500
+  })
+  return "Welcome, " + name + "! 500 credits have been added to your account";
+}
+
+async function getCredits(name) {
+  credits = await readFromDb("users", {'uid': name});
+  return new Promise(resolve => {
+    resolve(name + " has " + parseInt(credits['credits']) + " credits.");
   })
 }
 
-console.log(`${cfg.getTwitchUser()}`);
+function writeToDb(coll, dict) {
+  MongoClient.connect(uri, function (err, monclient) {
+    if (err) {
+      console.log('Error occurred while connecting to MongoDB Atlas...\n', err);
+    } else {
+      monclient.db("TABS").collection(coll).insertOne(dict, function (err, res) {
+        if (err) {
+          console.log('Error occurred while making account\n', err);
+        }
+      })
+      monclient.close();
+    }
+  });
+}
 
-const uri = `mongodb+srv://${cfg.getDbUser()}:${cfg.getDbToken()}@tabs-hoskn.mongodb.net/test?retryWrites=true`;
-MongoClient.connect(uri, function (err, monclient) {
-  if (err) {
-    console.log('Error occurred while connecting to MongoDB Atlas...\n', err);
-  } else {
-    const collection = monclient.db("TABS").collection("users");
-    /* collection.insertOne({
-      'uid': 'test',
-      'credits': 1
-    }, function (err, res) {
-      if (err) throw err;
-      console.log("Added");
-    }) */
-
-    // Define configuration options
-    const opts = {
-      identity: {
-        username: cfg.getTwitchUser(),
-        password: cfg.getTwitchToken()
-      },
-      connection: {
-        cluster: 'aws',
-        reconnect: true,
-      },
-      channels: [
-        cfg.getTwitchChannel()
-      ]
-    };
-
-    // Create a client with our options
-    const client = new tmi.client(opts);
-
-    // Register our event handlers (defined below)
-    client.on('message', onMessageHandler);
-    client.on('connected', onConnectedHandler);
-
-    // Connect to Twitch:
-    client.connect();
-
-    client.on('connected', (address, port) => {
-      client.action('chil_ttv', 'hello');
-    });
-
-    function onMessageHandler(target, context, msg, self) {
-      if (self) {
-        return;
-      } // Ignore messages from the bot
-
-      // Remove whitespace from chat message
-      const commandName = msg.trim();
-
-      if (commandName === '!dice') {
-        const num = rollDice();
-        client.say(target, `You rolled a ${num}`);
-        console.log(`* Executed ${commandName} command`);
-    
-      } else if (commandName == "!make") {
-        make(coll);
-        client.say(target, `Welcome`);
-    
+async function readFromDb(coll, dict) {
+  return new Promise(resolve => {
+    MongoClient.connect(uri, function (err, monclient) {
+      if (err) {
+        console.log('Error occurred while connecting to MongoDB Atlas...\n', err);
       } else {
-        console.log(`* Unknown command ${commandName}`);
+        monclient.db("TABS").collection(coll).findOne(dict, function (err2, res) {
+          if (err2) {
+            console.log('Error occurred while making account\n', err2);
+          } else {
+            console.log(res);
+            resolve(res);
+            //monclient.close();
+          }
+        })
       }
-    }
+    });
+  })
+  
+}
 
-    // Called every time the bot connects to Twitch chat
-    function onConnectedHandler(addr, port) {
-      console.log(`* Connected to ${addr}:${port}`);
-    }
+const opts = {
+  identity: {
+    username: cfg.getTwitchUser(),
+    password: cfg.getTwitchToken()
+  },
+  connection: {
+    cluster: 'aws',
+    reconnect: true,
+  },
+  channels: [
+    cfg.getTwitchChannel()
+  ]
+};
 
+// Create a client with our options
+const client = new tmi.client(opts);
 
-    monclient.close();
-  }
+// Register our event handlers (defined below)
+client.on('message', onMessageHandler);
+client.on('connected', onConnectedHandler);
 
+// Connect to Twitch:
+client.connect();
+
+client.on('connected', (address, port) => {
+  //client.action('chil_ttv', 'hello');
 });
+
+async function onMessageHandler(target, context, msg, self) {
+  if (self) {
+    return;
+  } // Ignore messages from the bot
+
+  var username = context['username'];
+  // Remove whitespace from chat message
+  const commandName = msg.trim().split(" ");
+
+  if (commandName[0] === '!dice') {
+    const num = rollDice();
+    client.say(target, `You rolled a ${num}`);
+    console.log(`* Executed ${commandName} command`);
+
+  } else if (commandName[0] == "!make") {
+    client.say(target, make(username));
+  } else if (commandName[0] == "!credits") {
+    response = await getCredits(username);
+    client.say(target, response);
+  }
+}
+
+// Called every time the bot connects to Twitch chat
+function onConnectedHandler(addr, port) {
+  console.log(`* Connected to ${addr}:${port}`);
+}
